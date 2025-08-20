1acae81eed911d227c5582036f657e692e66f487
RupakAryan <157547044+RupakAryan@users.noreply.github.com>
2025-08-20 20:54:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 API_ID = os.environ.get("API_ID", "29978901") #Replace With your api id
 API_HASH = os.environ.get("API_HASH", "500fc876c5356cf04ed3698912dc2edf") #Replace With your api hash
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7591531667:AAE12VDBEt5o04izLdG-WBvnozm1dDqzF1Y") #Replace With your bot token
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") #Replace With your bot token
